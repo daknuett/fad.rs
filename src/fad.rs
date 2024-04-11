b416d83f3bc7e7b41e9b746a50924fb053652a6a
Daniel Knuettel <Daniel Knuettel>
2024-04-11 17:23:17 +0200

    implemented basic arithmetics

--- src/fad.rs
+++ src/fad.rs
@@ -47,6 +47,47 @@ impl<C: Num> std::ops::Sub<C> for ForwardADNode<C> {
         }
     }
 }
+impl<C: Num + Copy> std::ops::Mul<C> for ForwardADNode<C> {
+    type Output = ForwardADNode<C>;
+
+    fn mul(self, rhs: C) -> ForwardADNode<C> {
+        ForwardADNode {
+            order0: self.order0 * rhs,
+            order1: self.order1 * rhs,
+        }
+    }
+}
+impl<C: Num + Copy> std::ops::Div<C> for ForwardADNode<C> {
+    type Output = ForwardADNode<C>;
+
+    fn div(self, rhs: C) -> ForwardADNode<C> {
+        ForwardADNode {
+            order0: self.order0 / rhs,
+            order1: self.order1 / rhs,
+        }
+    }
+}
+
+impl<C: Num + Copy> std::ops::Mul<ForwardADNode<C>> for ForwardADNode<C> {
+    type Output = ForwardADNode<C>;
+
+    fn mul(self, rhs: ForwardADNode<C>) -> ForwardADNode<C> {
+        ForwardADNode {
+            order0: self.order0 * rhs.order0,
+            order1: self.order1 * rhs.order0 + rhs.order1 * self.order0,
+        }
+    }
+}
+impl<C: Num + Copy> std::ops::Div<ForwardADNode<C>> for ForwardADNode<C> {
+    type Output = ForwardADNode<C>;
+
+    fn div(self, rhs: ForwardADNode<C>) -> ForwardADNode<C> {
+        ForwardADNode {
+            order0: self.order0 / rhs.order0,
+            order1: (self.order1 * rhs.order0 - rhs.order1 * self.order0) / rhs.order0 / rhs.order0,
+        }
+    }
+}
 
 //impl<C: Num> std::ops::Sub<ForwardADNode<C>> for C {
 //    type Output = ForwardADNode<C>;
@@ -96,6 +137,47 @@ impl std::ops::Add<ForwardADNode<f64>> for f64 {
         ForwardADNode {
             order0: self + rhs.order0,
             order1: rhs.order1,
+        }
+    }
+}
+
+impl std::ops::Div<ForwardADNode<f32>> for f32 {
+    type Output = ForwardADNode<f32>;
+
+    fn div(self, rhs: ForwardADNode<f32>) -> ForwardADNode<f32> {
+        ForwardADNode {
+            order0: self / rhs.order0,
+            order1: - self * rhs.order1 / rhs.order0.powi(2),
+        }
+    }
+}
+impl std::ops::Div<ForwardADNode<f64>> for f64 {
+    type Output = ForwardADNode<f64>;
+
+    fn div(self, rhs: ForwardADNode<f64>) -> ForwardADNode<f64> {
+        ForwardADNode {
+            order0: self / rhs.order0,
+            order1: - self * rhs.order1 / rhs.order0.powi(2),
+        }
+    }
+}
+impl std::ops::Mul<ForwardADNode<f32>> for f32 {
+    type Output = ForwardADNode<f32>;
+
+    fn mul(self, rhs: ForwardADNode<f32>) -> ForwardADNode<f32> {
+        ForwardADNode {
+            order0: self * rhs.order0,
+            order1: self * rhs.order1,
+        }
+    }
+}
+impl std::ops::Mul<ForwardADNode<f64>> for f64 {
+    type Output = ForwardADNode<f64>;
+
+    fn mul(self, rhs: ForwardADNode<f64>) -> ForwardADNode<f64> {
+        ForwardADNode {
+            order0: self * rhs.order0,
+            order1: self * rhs.order1,
         }
     }
 }
@@ -152,5 +234,44 @@ mod tests {
             assert_releq!(c.order0, -0.1_f32, 1e-5);
             assert_releq!(c.order1, -2.3_f32, 1e-5);
         }
+    }
+
+
+    #[test]
+    fn diff_mul()
+    {
+        let a = ForwardADNode{order0: 1.2, order1: 2.3};
+        let b = ForwardADNode{order0: 1.4, order1: 4.3};
+
+        {
+            let factor = 3f32;
+            let c = a / factor;
+            assert_releq!(c.order0, a.order0 / factor, 1e-5);
+            assert_releq!(c.order1, a.order1 / factor, 1e-5);
+        }
+        {
+            let factor = 3f32;
+            let c = a * factor;
+            assert_releq!(c.order0, a.order0 * factor, 1e-5);
+            assert_releq!(c.order1, a.order1 * factor, 1e-5);
+        }
+        {
+            let factor = 3f32;
+            let c = factor / a;
+            assert_releq!(c.order0, factor  / a.order0, 1e-5);
+            assert_releq!(c.order1, -4.791666666666666_f32, 1e-5);
+        }
+
+        {
+            let c = a * b;
+            assert_releq!(c.order0, 1.68_f32, 1e-5);
+            assert_releq!(c.order1, 8.38_f32, 1e-5);
+        }
+        {
+            let c = a / b;
+            assert_releq!(c.order0, 0.8571428571428572_f32, 1e-5);
+            assert_releq!(c.order1, -0.9897959183673468_f32, 1e-5);
+        }
+
     }
 }
